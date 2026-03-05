ec8d84058e5ac8358a6630af7121bd245376a355
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 09:23:00 +0530

    UC8: Palindrome using recursion

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,31 +1,15 @@
-class Node {
-    char data;
-    Node next;
-    Node(char data) { this.data = data; }
-}
-
 public class PalindromeCheckerApp {
 
+    public static boolean isPalindrome(String s, int start, int end) {
+        if(start >= end) return true;
+        if(s.charAt(start) != s.charAt(end)) return false;
+        return isPalindrome(s, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
-
-        String word = "rotor";
-        Node head = null;
-
-        for(char c : word.toCharArray()) {
-            Node node = new Node(c);
-            node.next = head;
-            head = node;
-        }
-
-        Node curr = head;
-        String reversed = "";
-        while(curr != null) {
-            reversed += curr.data;
-            curr = curr.next;
-        }
-
-        System.out.println(word + (word.equals(reversed) ? " is a Palindrome" : " is not a Palindrome"));
-
+        String word = "civic";
+        boolean result = isPalindrome(word, 0, word.length() - 1);
+        System.out.println(word + (result ? " is a Palindrome" : " is not a Palindrome"));
     }
 
 }
