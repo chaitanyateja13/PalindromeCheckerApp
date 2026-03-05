1d33770a776bf3bacdb2067b27767ee1e8489dd9
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 09:25:23 +0530

    UC9: Encapsulate palindrome logic in class

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,17 @@
-public class PalindromeCheckerApp {
+class PalindromeChecker {
 
-    public static boolean isPalindrome(String s, int start, int end) {
-        if(start >= end) return true;
-        if(s.charAt(start) != s.charAt(end)) return false;
-        return isPalindrome(s, start + 1, end - 1);
+    public boolean checkPalindrome(String word) {
+        return word.equals(new StringBuilder(word).reverse().toString());
     }
 
+}
+
+public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        String word = "civic";
-        boolean result = isPalindrome(word, 0, word.length() - 1);
-        System.out.println(word + (result ? " is a Palindrome" : " is not a Palindrome"));
+        PalindromeChecker checker = new PalindromeChecker();
+        String word = "madam";
+        System.out.println(word + (checker.checkPalindrome(word) ? " is a Palindrome" : " is not a Palindrome"));
     }
 
 }
