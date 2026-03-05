8d0fb9d9c2a42e0e4352eb6237fde3abf16217cf
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 09:34:06 +0530

    UC13: Test multiple strings

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,28 +7,26 @@ interface PalindromeStrategy {
     boolean check(String word);
 }
 
-// All strategies encapsulated
-class PalindromeStrategies {
-
-    static class StackStrategy implements PalindromeStrategy {
-        public boolean check(String word) {
-            Stack<Character> stack = new Stack<>();
-            for (char c : word.toCharArray()) stack.push(c);
-            String reversed = "";
-            while (!stack.isEmpty()) reversed += stack.pop();
-            return word.equals(reversed);
-        }
+// Stack-based strategy
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String word) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : word.toCharArray()) stack.push(c);
+        String reversed = "";
+        while (!stack.isEmpty()) reversed += stack.pop();
+        return word.equals(reversed);
     }
+}
 
-    static class DequeStrategy implements PalindromeStrategy {
-        public boolean check(String word) {
-            Deque<Character> deque = new ArrayDeque<>();
-            for (char c : word.toCharArray()) deque.add(c);
-            while (deque.size() > 1) {
-                if (deque.removeFirst() != deque.removeLast()) return false;
-            }
-            return true;
+// Deque-based strategy
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : word.toCharArray()) deque.add(c);
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) return false;
         }
+        return true;
     }
 }
 
@@ -36,12 +34,19 @@ class PalindromeStrategies {
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String word = "level";
+        String[] words = {"madam", "racecar", "hello", "noon", "level"};
 
-        PalindromeStrategy strategy1 = new PalindromeStrategies.StackStrategy();
-        System.out.println("StackStrategy: " + word + " -> " + strategy1.check(word));
+        StackStrategy stack = new StackStrategy();
+        DequeStrategy deque = new DequeStrategy();
 
-        PalindromeStrategy strategy2 = new PalindromeStrategies.DequeStrategy();
-        System.out.println("DequeStrategy: " + word + " -> " + strategy2.check(word));
+        System.out.println("Testing multiple strings with StackStrategy:");
+        for(String w : words) {
+            System.out.println(w + " -> " + stack.check(w));
+        }
+
+        System.out.println("\nTesting multiple strings with DequeStrategy:");
+        for(String w : words) {
+            System.out.println(w + " -> " + deque.check(w));
+        }
     }
 }
