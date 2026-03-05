49caee9581ceddfcc502d80e82c25f28d007ab21
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 09:20:01 +0530

    UC4: Two-pointer character array palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,15 +2,21 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "racecar";
-        String reversed = "";
+        String word = "level";
+        char[] arr = word.toCharArray();
+        boolean isPalindrome = true;
 
-        for(int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+        int start = 0, end = arr.length - 1;
+        while(start < end) {
+            if(arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        System.out.println(word + (word.equals(reversed) ? " is a Palindrome" : " is not a Palindrome"));
-
+        System.out.println(word + (isPalindrome ? " is a Palindrome" : " is not a Palindrome"));
     }
 
 }
