f4c7353836c5bd456a4e1cf2dc329a7f6872af18
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 09:25:56 +0530

    UC10: Strategy pattern setup

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,32 @@
-class PalindromeChecker {
+interface PalindromeStrategy {
+    boolean check(String word);
+}
 
-    public boolean checkPalindrome(String word) {
-        return word.equals(new StringBuilder(word).reverse().toString());
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String word) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for(char c : word.toCharArray()) stack.push(c);
+        String rev = "";
+        while(!stack.isEmpty()) rev += stack.pop();
+        return word.equals(rev);
     }
+}
 
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String word) {
+        java.util.Deque<Character> deque = new java.util.ArrayDeque<>();
+        for(char c : word.toCharArray()) deque.add(c);
+        while(deque.size() > 1) {
+            if(deque.removeFirst() != deque.removeLast()) return false;
+        }
+        return true;
+    }
 }
 
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-        PalindromeChecker checker = new PalindromeChecker();
-        String word = "madam";
-        System.out.println(word + (checker.checkPalindrome(word) ? " is a Palindrome" : " is not a Palindrome"));
+        String word = "level";
+        PalindromeStrategy strategy = new StackStrategy(); // choose dynamically
+        System.out.println(word + (strategy.check(word) ? " is a Palindrome" : " is not a Palindrome"));
     }
-
 }
