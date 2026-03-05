0933aaefb811c9a976bfbad1bf7336ff7e087d8b
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-05 09:18:58 +0530

    UC3: Reverse string using loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,18 +2,15 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("   PALINDROME CHECKER APP");
-        System.out.println("   Version 1.0");
-        System.out.println("=================================");
+        String word = "racecar";
+        String reversed = "";
 
-        String word = "madam";
-
-        if(word.equals(new StringBuilder(word).reverse().toString())) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is not a Palindrome");
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
         }
 
+        System.out.println(word + (word.equals(reversed) ? " is a Palindrome" : " is not a Palindrome"));
+
     }
+
 }
